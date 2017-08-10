9a944a74a93a2dd491d26ddbeb36a91f604f84c0
Idesmi <Idesmi@users.noreply.github.com>
2017-08-10 10:49:14 +0000

    Update lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -1,16 +1,16 @@
 # Release name
-PRODUCT_RELEASE_NAME := GT-S7270
+PRODUCT_RELEASE_NAME := SM-G313HN
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/logan/device_logan.mk)
+$(call inherit-product, device/samsung/vivaltonfc3g/device_vivaltonfc3g.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := logan
-PRODUCT_NAME := lineage_logan
+PRODUCT_DEVICE := vivaltonfc3g
+PRODUCT_NAME := lineage_vivaltonfc3g
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
-PRODUCT_MODEL := GT-S7270
+PRODUCT_MODEL := SM-G313HN
 PRODUCT_CHARACTERISTICS := phone
